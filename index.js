935935e797661bfff0ef84f962ff68cdc2de060e
ZaremskiUladzislau <grazhdanskijv@mail.ru>
2024-01-10 22:44:50 +0300

    #1

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/**
+ * Adds two numbers together.
+ *
+ * @param {number} a - The first number to add.
+ * @param {number} b - The second number to add.
+ * @return {number} The sum of the two numbers.
+ */
 function sum(a, b) {
     return a + b;
+}
+/**
+ * Calculates the subtraction of two numbers.
+ *
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @return {number} The result of subtracting b from a.
+ */
+function sub(a, b) {
+    return a - b;
 }
